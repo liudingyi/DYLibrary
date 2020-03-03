8d8bcd92108da69d9f025b4fa58b0ca7326f350f
liudingyi <1013731419@qq.com>
2020-03-03 12:36:23 +0800

    update

--- sample/src/main/java/com/ldy/sample/App.java
+++ sample/src/main/java/com/ldy/sample/App.java
@@ -2,7 +2,7 @@ package com.ldy.sample;
 
 import android.app.Application;
 import android.content.Context;
-import android.support.multidex.MultiDex;
+import androidx.multidex.MultiDex;
 
 public class App extends Application {
 
