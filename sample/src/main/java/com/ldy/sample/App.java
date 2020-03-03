cb6ec481fe212bd8bc9db0fde2509c74bf4d126b
liudingyi <1013731419@qq.com>
2020-03-03 12:31:20 +0800

    update

---
+++ sample/src/main/java/com/ldy/sample/App.java
@@ -0,0 +1,14 @@
+package com.ldy.sample;
+
+import android.app.Application;
+import android.content.Context;
+import android.support.multidex.MultiDex;
+
+public class App extends Application {
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(base);
+    }
+}
